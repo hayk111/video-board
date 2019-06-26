43708e55f448ad2fa8033f50827f061e63d6b3e5
haykatoyan111 <hayk@simplytechnologies.com>
2019-06-26 18:58:17 +0400

    Added moment and removed environment files linting

--- src/app/video-grid.component.ts
+++ src/app/video-grid.component.ts
@@ -1,28 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 import { environment } from 'src/environments/environment.prod';
 import { map } from 'rxjs/operators';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './video-grid.component.html',
   styleUrls: ['./video-grid.component.scss']
 })
-export class VideoGridComponent {
+export class VideoGridComponent implements OnInit {
   @ViewChild('agGrid', {static: false}) agGrid: AgGridAngular;
   title = 'video-board';
-  private isFullWidthCell;
-  private fullWidthCellRenderer;
   rowData: any;
 
   columnDefs = [
     {
       cellRenderer: ({value}) => {
         const {url, height, width} = value;
-        return `<img src="${url}" width="${width}" height="${height}">`;
-      }, headerName: '', field: 'thumbnails', sortable: true, filter: true, checkboxSelection: true, width: 300},
-    {headerName: 'Published on', field: 'publishedAt', sortable: true, filter: true, editable: true, width: 300 },
+        return `<img src='${url}' width='${width}' height='${height}'>`;
+      }, headerName: '', field: 'thumbnails', sortable: true, filter: true, checkboxSelection: true, width: 300
+    },
+    {
+      cellRenderer: ({value}) => {
+        return `<time>${moment(value).format('YYYY-MM-DD, h:mm:ss a')}</time>`;
+      }, headerName: 'Published on', field: 'publishedAt', sortable: true, editable: true, filter: true, width: 300
+    },
     {headerName: 'Video Title', field: 'title', sortable: true, filter: true, editable: true, width: 300 },
     {headerName: 'Description', field: 'description', sortable: true, filter: true, editable: true, width: 300 },
   ];
@@ -44,12 +49,11 @@ export class VideoGridComponent {
           }));
         })
       );
-    console.log("TCL: VideoGridComponent -> ngOnInit -> this.rowData", this.rowData)
   }
 
 /*   getSelectedRows() {
     const selectedNodes = this.agGrid.api.getSelectedNodes();
-    console.log("TCL: VideoGridComponent -> getSelectedRows -> selectedNodes", selectedNodes)
+    console.log('TCL: VideoGridComponent -> getSelectedRows -> selectedNodes', selectedNodes)
     const selectedData = selectedNodes.map( node => node.data );
     const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
